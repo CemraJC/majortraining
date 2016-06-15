b802364b7e51f748c90b5b6b1bfb9d1082cc5df0
Jason Cemra <cemrajc@gmail.com>
2016-06-15 13:18:31 +1000

    Downgraded Scoring Expectations

--- main.js
+++ main.js
@@ -74,61 +74,61 @@ var save_file = new (function() {
                 format: "2",
                 info: "2 digit numbers only",
                 highscore: 0,
-                pass: 120
+                pass: 75
             },
             2: {
                 format: "3",
                 info: "3 digit numbers only",
                 highscore: 0,
-                pass: 150
+                pass: 75
             },
             3: {
                 format: "2-3",
                 info: "2 and 3 digit numbers",
                 highscore: 0,
-                pass: 140
+                pass: 75
             },
             4: {
                 format: "3|3",
                 info: "2 groups of 3 digit numbers",
                 highscore: 0,
-                pass: 180
+                pass: 75
             },
             5: {
                 format: "4",
                 info: "4 digit numbers only",
                 highscore: 0,
-                pass: 110
+                pass: 75
             },
             6: {
                 format: "2-4|2-4",
                 info: "2 groups of 2-4 digit numbers",
                 highscore: 0,
-                pass: 150
+                pass: 75
             },
             7: {
                 format: "2-3|2-3|2-3|2-3",
                 info: "4 groups of 2-3 digit numbers",
                 highscore: 0,
-                pass: 220
+                pass: 75
             },
             8: {
                 format: "4|3|3",
                 info: "Phone numbers",
                 highscore: 0,
-                pass: 200
+                pass: 75
             },
             9: {
                 format: "5|5|5|5|5",
                 info: "Credit card numbers",
                 highscore: 0,
-                pass: 230
+                pass: 75
             },
             10: {
                 format: "40",
                 info: "40 digit mega numbers",
                 highscore: 0,
-                pass: 200
+                pass: 75
             }
         },
     }
