0aeb44e2abc1fbe91c4b41bc2743b4429222ce44
Jason Cemra <cemrajc@gmail.com>
2016-06-29 12:18:26 +1000

    Added: Built website

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/*
+
+    Kick off!
+    The order of these matters - some rely on others, so be careful
+    Also, try not to make circular depenndencies - something has to be defined first.
+
+ */
+
+document.addEventListener("DOMContentLoaded", function(){
+    elements.init();
+    fontScale.init();
+    save_file.init();
+    display.init();
+    inputs.init();
+    WordGenerator.init();
+    game.init();
+});
