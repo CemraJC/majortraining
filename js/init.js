6b0c4c20f3433fa5704bae8f11b6105b06431ce4
Jason Cemra <cemrajc@users.noreply.github.com>
2017-10-07 14:24:33 +1100

    Added Level 0

--- js/init.js
+++ js/init.js
@@ -2,9 +2,10 @@
 
     Kick off!
     The order of these matters - some rely on others, so be careful
-    Also, try not to make circular depenndencies - something has to be defined first.
+    Also, try not to make circular dependencies - something has to be defined first.
 
  */
+
 
 document.addEventListener("DOMContentLoaded", function(){
     elements.init();
